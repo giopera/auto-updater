fb94fa2ac4a738b31969b4d52fe6068bf39940ba
giopera <perigio735@gmail.com>
2025-01-10 19:07:46 +0100

    First steps with flask

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
 @app.route('/webhook', methods=['POST'])
 def webhook_receiver():
-    data = request.json  # Get the JSON data from the incoming request
-    # Process the data and perform actions based on the event
+    data = request.json
     print("Received webhook data:", data)
     return jsonify({'message': 'Webhook received successfully'}), 200
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=11212, host="0.0.0.0")
